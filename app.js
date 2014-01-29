468da6379b460cc63222aec95b69c4a5282a91d6
vagrant <vagrant@precise32.(none)>
2014-01-29 23:00:51 +0000

    node.js lab

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var path = require('path');
 var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
+var project = require('./routes/project');
 // Example route
 // var user = require('./routes/user');
 
@@ -36,6 +37,9 @@ if ('development' == app.get('env')) {
 
 // Add routes here
 app.get('/', index.view);
+app.get('/project', project.viewProject);
+app.get('/project/:name', project.viewProject);
+
 // Example route
 // app.get('/users', user.list);
 
